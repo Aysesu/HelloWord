a1a1535fe287a53cf2f67a1d0635dc87bebe1fac
AysesuBozyel <AysesuBozyel@0-208-14>
2020-09-25 12:01:39 +0200

    FizzBuzz

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,22 @@
 
 public class HelloWorld {
-
+static int counter = 0 ;
 	public static void main(String[] args) {
-	System.out.println("HelloWorld");
-
+	
+	
+		while (counter<=100) {
+			if (counter%3==0 && counter%5==0) {
+			System.out.println ("FizzBuzz"); }
+			if(counter%3==0) {System.out.println("Fizz");}
+		else { 
+	if(counter%5==0) {System.out.println("Buzz");} 
+	
+	else {System.out.println(counter);}
+	
+			}
+			counter++; 
+		}
 	}
-
 }
+
+
